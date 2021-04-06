09bea513963cda92c8056cb183db0956dcf562e1
freddy DC <freddydiaz.inc@gmail.com>
2021-04-05 20:02:14 -0500

    Create NodeJS server

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Shirt",
+            category: "Nike Shirts",
+            image: "/images/product_1.jpg",
+            price: 80,
+            countInStock: 12,
+            brand: "Nike",
+            rating: 3.6,
+            numReviews: 10,
+            description: "High quality product",
+        },
+        {
+            _id: "2",
+            name: "Pant",
+            category: "Adidas Pants",
+            image: "/images/rust.png",
+            price: 50,
+            countInStock: 0,
+            brand: "Adidas",
+            rating: 4,
+            numReviews: 90,
+            description: "High quality product",
+        },
+        {
+            _id: "3",
+            name: "Shoes",
+            category: "Gucci Shoes",
+            image: "/images/rust.png",
+            price: 100,
+            countInStock: 7,
+            brand: "Gucci",
+            rating: 3.5,
+            numReviews: 8,
+            description: "High quality product",
+        },
+    ],
+};
+
+export default data;

79f92710ceba5bc448ed8ec1f4245060503f1af3
freddydc <freddydiaz.liv@gmail.com>
2021-06-05 18:01:18 -0500

    Create Shipping Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,27 +1,29 @@
 import Axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
-/*
+/* ==> (add-to-cart)  action <==
 ? Defining an action return (async) function.
-* getState: make possible to (get access), to state of (redux store).
-
-* ==> LOCAL STORAGE <==
-  ? Maintain saved persistent data (cart items added),
-  * ... if web page is (refreshed) again.
-  * * Local Storage: setItem('key name custom', value: string)
-  ? Get access: (getState().cart.cartItems) from redux thunk,
-  * ... by redux (store).
+* getState: make possible to (get access) to state of (redux store).
+? == Local Storage ==
+* Maintain saved persistent data (cart items added)
+*   if browser page is (refreshed-again).
+* Local Storage: setItem('key name custom', value: string)
+? Get access: (getState().cart.cartItems)
+?   from redux thunk by redux (store).
 */
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
-  //! Info
-  console.log("Add To Cart Action qty:", qty);
-
+  //! Info.
+  // console.log(`Add to cart action quantity: ${qty}`);
   dispatch({
-    /*
-    ? (payload): important
-    * (id) = product
-    * (qty): important
+    /* ==> Details <==
+    ? - (payload): important.
+    * - (id) = product.
+    * - (qty = quantity): important.
     */
     type: CART_ADD_ITEM,
     payload: {
@@ -36,10 +38,17 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
-//* Remove Cart Item.
+/* ==> (remove-cart-item) action <== */
 export const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: productId });
-
-  //? Update (Local Storage) data.
+  //? ==> Update (Local-Storage) data information.
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+/* ==> ( save-shipping-address ) action
+? - Accept ( dispatch ) parameter from ( redux-thunk ).
+*/
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
 };
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
 /* ==> (cart) actions <==
 * ==> case: CART_ADD_ITEM <==
@@ -47,6 +51,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
       };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return { ...state, shippingAddress: action.payload };
     default:
       return state;
   }

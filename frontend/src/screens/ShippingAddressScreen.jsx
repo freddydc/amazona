fbfd91bf2a273dc00a9c04fa5f50a09d4d0bf68b
freddydc <freddydiaz.liv@gmail.com>
2021-06-19 12:17:36 -0500

    Fix Default State: Shipping Address

--- frontend/src/screens/ShippingAddressScreen.jsx
+++ frontend/src/screens/ShippingAddressScreen.jsx
@@ -18,11 +18,13 @@ export default function ShippingAddressScreen(props) {
     props.history.push("/signin");
   }
 
-  const [fullName, setFullName] = useState(shippingAddress.fullName);
-  const [address, setAddress] = useState(shippingAddress.address);
-  const [city, setCity] = useState(shippingAddress.city);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country);
+  const [fullName, setFullName] = useState(shippingAddress.fullName || "");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
 
   const dispatch = useDispatch();
   /* ==> (save-shipping-address) dispatch <==
